4a4e9f15f96e6f45091cb368284680eb4b08c2ce
zrinGithub <806488823@qq.com>
2020-11-10 15:14:40 +0800

    add

---
+++ AlibbaCloud+SpringCloud/code/alibaba-demo/gateway/src/main/java/com/zr/gateway/filter/UserGlobalFilter.java
@@ -0,0 +1,35 @@
+package com.zr.gateway.filter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * Description:
+ *
+ * @author zhangr
+ * 2020/11/5 14:21
+ */
+//@Component
+public class UserGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getHeaders().getFirst("token");
+
+        if (StringUtils.isBlank(token)) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
